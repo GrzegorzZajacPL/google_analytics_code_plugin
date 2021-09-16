34ffe55b0f377f716ce2372b76b97014eaa56ae3
GrzegorzZajacPL <90873837+GrzegorzZajacPL@users.noreply.github.com>
2021-09-17 00:26:33 +0200

    Add files via upload

---
+++ google-analytics-code-plugin.php
@@ -0,0 +1,42 @@
+<?php
+
+
+/*
+Plugin Name: Wtyczka Google Analytics na GrzegorzZajac.pl
+Description: Funkcjonalność bloga, niezwiązana z wyglądem motywu.
+Version: 0.1-16.09.2021
+License: GPL
+Author: Grzegorz Zając
+Author URI: https://grzegorzzajac.pl
+*/
+
+
+/*
+* Dodanie w sekcji head
+*/
+
+add_action( 'wp_head', 'my_own_analytics', 20 );
+function my_own_analytics() { ?>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-SPE3HW2GPY"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-SPE3HW2GPY');
+    </script>
+    <?php
+}
+
+
+
+
+
+
+
+
+
+
+
+
